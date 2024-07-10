b6bd8df125abf6710456c17fd15e89cbbda3c812
Shreyash Jain <shreyashjain41@gmail.com>
2024-07-10 20:53:51 +0530

    third commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import portfolio from './../assets/portfolioImg.jpg'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+export default function Home(){
+    return(
+        <>
+            <div>
+                <div>
+                    <h2>I am React Js Developer</h2>
+                    <p></p>
+                </div>
+            </div>
+        </>
+    )
+}
