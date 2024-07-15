bf6e70909e1a9e824aa94b293c4213b3e4f507e8
Shreyash Jain <shreyashjain41@gmail.com>
2024-07-15 20:50:29 +0530

    portfolio commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     fontFamily : {
       heading: ['Playwrite CL']
     }
