23aaa01db7ded42c25ab5eb755896209a1686a7d
shreyash.jain <shreyash.jain@igniculuss.com>
2024-10-12 02:22:45 +0530

    ok+

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
+
 
 import './App.css'
 import About from './components/About'
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <>
-    <Analytics />
+    
     <Notification/>
     <Navbar/>
     <Home/>
